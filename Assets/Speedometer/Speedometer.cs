96701fdd30ffbefb8318ee9f6e09c1545c88489f
Dani <45202338+xsiro@users.noreply.github.com>
2021-12-18 16:09:48 +0100

    such a nice speedometer

--- Assets/Speedometer/Speedometer.cs
+++ Assets/Speedometer/Speedometer.cs
@@ -1,78 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Speedometer : MonoBehaviour {
+public class Speedometer : MonoBehaviour
+{
+    public Rigidbody target;
 
-    private const float MAX_SPEED_ANGLE = -20;
-    private const float ZERO_SPEED_ANGLE = 230;
+    public float maxSpeed = 0.0f; // The maximum speed of the target ** IN KM/H **
 
-    private Transform needleTranform;
-    private Transform speedLabelTemplateTransform;
+    public float minSpeedArrowAngle;
+    public float maxSpeedArrowAngle;
 
-    private float speedMax;
-    private float speed;
+    [Header("UI")]
+    public Text speedLabel; // The label that displays the speed;
+    public RectTransform arrow; // The arrow in the speedometer
 
-    private void Awake() {
-        needleTranform = transform.Find("needle");
-        speedLabelTemplateTransform = transform.Find("speedLabelTemplate");
-        speedLabelTemplateTransform.gameObject.SetActive(false);
+    private float speed = 0.0f;
+    private void Update()
+    {
+        // 3.6f to convert in kilometers
+        // ** The speed must be clamped by the car controller **
+        speed = target.velocity.magnitude * 3.6f;
 
-        speed = 0f;
-        speedMax = 200f;
-
-        //CreateSpeedLabels();
-    }
-
-    private void Update() {
-        HandlePlayerInput();
-
-        //speed += 30f * Time.deltaTime;
-        //if (speed > speedMax) speed = speedMax;
-
-        needleTranform.eulerAngles = new Vector3(0, 0, GetSpeedRotation());
-    }
-
-    private void HandlePlayerInput() {
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            float acceleration = 80f;
-            speed += acceleration * Time.deltaTime;
-        } else {
-            float deceleration = 20f;
-            speed -= deceleration * Time.deltaTime;
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow)) {
-            float brakeSpeed = 100f;
-            speed -= brakeSpeed * Time.deltaTime;
-        }
-
-        speed = Mathf.Clamp(speed, 0f, speedMax);
-    }
-
-    //private void CreateSpeedLabels() {
-    //    int labelAmount = 10;
-    //    float totalAngleSize = ZERO_SPEED_ANGLE - MAX_SPEED_ANGLE;
-
-    //    for (int i = 0; i <= labelAmount; i++) {
-    //        Transform speedLabelTransform = Instantiate(speedLabelTemplateTransform, transform);
-    //        float labelSpeedNormalized = (float)i / labelAmount;
-    //        float speedLabelAngle = ZERO_SPEED_ANGLE - labelSpeedNormalized * totalAngleSize;
-    //        speedLabelTransform.eulerAngles = new Vector3(0, 0, speedLabelAngle);
-    //        speedLabelTransform.Find("speedText").GetComponent<Text>().text = Mathf.RoundToInt(labelSpeedNormalized * speedMax).ToString();
-    //        speedLabelTransform.Find("speedText").eulerAngles = Vector3.zero;
-    //        speedLabelTransform.gameObject.SetActive(true);
-    //    }
-
-    //    needleTranform.SetAsLastSibling();
-    //}
-
-    private float GetSpeedRotation() {
-        float totalAngleSize = ZERO_SPEED_ANGLE - MAX_SPEED_ANGLE;
-
-        float speedNormalized = speed / speedMax;
-
-        return ZERO_SPEED_ANGLE - speedNormalized * totalAngleSize;
+        if (speedLabel != null)
+            speedLabel.text = ((int)speed) + " km/h";
+        if (arrow != null)
+            arrow.localEulerAngles =
+                new Vector3(0, 0, Mathf.Lerp(minSpeedArrowAngle, maxSpeedArrowAngle, speed / maxSpeed));
     }
 }
