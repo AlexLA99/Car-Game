67f4a054f9b64d64cdcd5ed1fda88e57427d5533
Dani <45202338+xsiro@users.noreply.github.com>
2022-01-08 13:44:39 +0100

    cositas

---
+++ Assets/Scripts/ButtonOptions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonOptions : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
+    }
+}
