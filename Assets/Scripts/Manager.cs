57914c928592db48a2e0eb7c187e39f3b8ded70a
AlexLA99 <45202060+AlexLA99@users.noreply.github.com>
2022-01-16 23:36:19 +0100

    gameManager creator

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class Manager : MonoBehaviour
+{
+    void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if (scene.buildIndex == 1)
+        {
+            PhotonNetwork.Instantiate("PhotonPrefabs/GameManager", Vector3.zero, Quaternion.identity);
+            PhotonNetwork.Instantiate("PhotonPrefabs/GameManager", Vector3.zero, Quaternion.identity);
+        }
+    }
+}
